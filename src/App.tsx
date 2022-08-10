0a7b7e47a479f063daccf53f1526bcd3ddd40000
zubair <zubairfloat@gmail.com>
2022-08-11 01:36:26 +0500

    add layout init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Routes from './routes/index'
+import {useDispatch , useSelector} from 'react-redux'
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  const {layoutMode} = useSelector((state: any) => ({
+    layoutMode: state.layout.layoutMode
+  }))
+
+  useEffect(() => {
+    const getLayoutMode = localStorage.getItem("layoutMode");
+    if(getLayoutMode) {
+      
+    }
+
+  }, [layoutMode , dispatch])
+  console.log(layoutMode)
   return  <Routes />
 }
 
